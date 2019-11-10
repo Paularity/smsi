5ddc813629b6dab5bc17c3e4e18150c0f5c90da2
Christian Decembrana <christiandecembrana1@gmail.com>
2019-11-10 21:45:36 +0800

    asd

---
+++ RazorWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RazorWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RazorWeb/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿//Chan's scripts will be included here
+//for now vanilla javascript will be added and soon will be converted to angular's typescript code
+console.log(" chan's script is now working...");
+
+function toggleSidebar()
+{    
+    //toggle full sidebar
+    $('#sidebar')
+        .css({ transition: '.5s' })
+        .toggleClass('sidebar-show', 300);
+
+    //toggle title of each menu item
+    if ($('#sidebar').hasClass('sidebar-show')) {
+        $('span.sidebar-menu-title').css({display: 'inline-block'});
+    }
+    else
+    {
+        $('span.sidebar-menu-title').css({ display: 'none' });  
+    }
+}
